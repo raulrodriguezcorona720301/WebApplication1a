54b5539d0410137cb1a232bae35ebe5878f5840e
Raul Rodriguez <proyecto.seis@mirumagency.com>
2018-10-22 17:44:59 -0500

    prueba unitaria

---
+++ UnitTestProject1/BusinessTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Entities;
+using System.Collections.Generic;
+using WebApplication1.Business;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+namespace WebApplication1.UnitTestProject1
+{
+    [TestClass]
+    public class BusinessTest
+    {
+        [TestMethod]
+        public void CtrlProduct_getCatalogTest()
+        {
+            CtrlProduct ctrlProduct = new CtrlProduct();
+            List<EntProduct> result = ctrlProduct.getCatalog(null);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Count, 3);
+        }
+    }
+}
--- UnitTestProject1/LoginControllerTest.cs
+++ UnitTestProject1/LoginControllerTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebApplication1.Entities;
 using WebApplication1.Controllers;
@@ -13,12 +11,36 @@ namespace WebApplication1.UnitTestProject1
     public class LoginControllerTest
     {
         [TestMethod]
-        public void IndexText()
+        public void setFnTest()
+        {
+            LoginController lc = new LoginController();
+            lc.ControllerContext = new ControllerContext();
+            //Add
+            var result = lc.setFn();
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Content.ToString(), "xyz");
+        }
+
+        [TestMethod]
+        public void IndexTest()
         {
             //Arrange
             LoginController lc = new LoginController();
+            lc.ControllerContext = new ControllerContext();
+            //Add
+            var result = lc.Index() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexTestParam()
+        {
+            //Arrange
+            LoginController lc = new LoginController();
+            lc.ControllerContext = new ControllerContext();
             EntLoginVm loginVm = new EntLoginVm() { fn="x", PasswordSup="123", tokenId="12345", UsuarioSup="juan", yesNo="true" };
-            //Session["tokenId"] = "12345";
             //Add
             var result = lc.Index(loginVm) as ViewResult;
             //Assert
